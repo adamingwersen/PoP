d62e3c08acb917e9d921f7febc07dd819ae94167
adamingwersen <adamlinnemann@gmail.com>
2016-09-17 13:34:41 +0200

    init

---
+++ Assignments/2/us_2.fsx
@@ -0,0 +1,64 @@
+// PoP Ugeseddel 2 
+
+  // 2ø.1
+let pi = 3.14;;
+let euler = 2.78;;
+let combi = pi + euler;;
+printfn "Opgave : 2ø.1"
+printfn "float: %f" combi
+
+  // 2ø.2
+printfn "\n Opgave 2ø.2 : \n aString = 'b', {'a'}; kan anvende \n
+ b, ba, baa"
+
+  // 2ø.3
+printfn "\n Opgave 2ø.3 \n EBNF'en kan eksempelvist returnereen expression alas: \n
+ 1+1, 1-9, 2988881*6, 1/666, 666/1, etc."
+
+  // 2ø.4
+let string1 = "hello";;
+let string0 = " ";;
+let string2 = "world";;
+let conc = string1 + string0 + string2;;
+printfn "\n Opgave : 2ø.4"
+printfn "%s" conc
+
+  // 2ø.5
+let flyd = (float 3) + 1.0;;
+printfn "\n Opgave : 2ø.5"
+printfn "float: %f"  flyd
+  // 2ø.6
+
+  // 2ø.7
+printfn "\n Opgave : 2ø.7"
+printfn "decimal: %d" 47
+printfn "hexadecimal: %x" 47
+printfn "octal: %o" 47
+printfn "float: %f" (float 47)
+
+  // 2ø.8
+
+
+  // 2ø.9
+let bytes = 164uy + 230uy;;
+printfn "\n Opgave : 2ø.9" 
+printfn "byte: %i" bytes
+printfn "\n uy står for unsigned byte
+spændet for uy er 0-255 {256}, hvorfor der \n returneres 138, da (165+230)-256 = 138"
+
+  // 2ø.10
+printfn "\n Opgave : 2ø.10 \nhello\nworld\n"
+printfn "   Udtrykket angiver ny linje eller linebreak"
+
+
+  // 2ø.11
+let litStr = @"\n";;
+printfn "\n Opgave 2ø.12 : Vi skal bruge verbatim \n %s" litStr
+
+  // 2ø.12
+
+
+  // 2ø.13
+let alpStr = "abcdef";;
+printfn "\n Opgave 2ø.13 : %s" alpStr.[1..3] 
+
