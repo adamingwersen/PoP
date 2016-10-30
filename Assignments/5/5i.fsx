eae119716148d96709a44a0f170e3cda640af7b5
adamingwersen <adamlinnemann@gmail.com>
2016-10-30 15:15:07 +0100

    add 7pdf

--- Assignments/5/5i.fsx
+++ Assignments/5/5i.fsx
@@ -10,11 +10,20 @@ printfn "%A" (concat listOfList)
 /// <summary> Her vil jeg anvende funktionerne tryFind og map </summary>
 let floatList = 2.0::3.11::4.91::1.11::84.13::[]
 
-let rec findFloat x = function
-    | [] -> None
-    | x::xs -> List.tryFind(fun x -> x = x) xs
+let gennemsnit list =
+    (List.fold (fun acc elem -> acc + float elem) 0.0 list / float list.Length)
 
-printfn "%A" (findFloat floatList)
+let gnst list =
+    match list with
+    | [] -> None
+    | l::ls -> ((fun acc elem -> acc + float elem) 0.0 list / float list.Length)::ls
+    | _ -> None
+
+let rec rex list =
+    match list with
+    | list::slist -> 
+
+printfn "%A" (List.average floatList)
 
 /// <summary> i5.3 </summary>
 
