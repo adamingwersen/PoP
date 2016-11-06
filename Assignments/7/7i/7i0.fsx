46b77c21ac3c51ebe0dde12a2c238a441ddd1d30
adamingwersen <adamlinnemann@gmail.com>
2016-11-06 19:07:51 +0100

    fixed a7

--- Assignments/7/7i/7i0.fsx
+++ Assignments/7/7i/7i0.fsx
@@ -5,47 +5,47 @@ let arrayEx = [|1;2;4;7;10;9|]
 
 printfn "Vi betragter array : %A  af længde %d \n" arrayEx arrayEx.Length
 
-/// <remarks> exception: Defines an exception </remarks>
-exception IfIndexInvalid of string
-/// </summary> SafeIndexIf : A function, which takes two inputs and evaluates with if/else structure. If the provided index (x) is not within the length of the array (A) an error is thrown. This function utilizes the raise(), which halts the program if encountered. </summary>
+
+/// </summary> SafeIndexIf : A function, which takes two inputs and evaluates with if/else structure. If the provided index (x) is not within the length of the array (A) an error is not thrown. This function utilizes the default-value of an array if out-of-bounds. </summary>
 /// <param name="A"> A is an Array </param>
 /// <param name="x"> x is an integer, which specifies the index of the array </param>
-let SafeIndexIf (A : 'a[]) (x : int) =
-    if x >= (Array.length A) then
-        raise (IfIndexInvalid "Index is of invalid size")
+let safeIndexIf (A : 'a[]) x =
+    if ((x >= (Array.length A)) || (x < 0))  then
+        Unchecked.defaultof<'a>
     else
         A.[x]
-
-Console.WriteLine "Vælg x af type int til 'IfIndexinvalid'"
+ 
+        
+Console.WriteLine "Vælg x af type int til 'safeIndexIf'"
 /// <param name="indexvalue"> indexValue acts as input to (x) </param>
 /// <remarks> Provide the user with an option of entering the desired index </remarks>
 let mutable indexValue = int (Console.ReadLine())
-printfn "A.[%d]:  %A" indexValue (SafeIndexIf arrayEx indexValue)
+printfn "A.[%d]:  %A" indexValue (safeIndexIf arrayEx indexValue)
 
 /// <summary> SafeIndexTry : A function, which takes two inputs and evaluates using the try/with structure. Here if index is out of bounds, the runtime error is thrown using the failwith-statement. This halts the execution of proceeding code. </summary>
 /// <param name="A"> A is an Array </param>
 /// <param name="x"> x is an integer, which specifies the index of the array </param>
-let SafeIndexTry (A : 'a[]) x =
+let safeIndexTry (A : 'a[]) x =
     try
         A.[x]
     with
         _ -> failwith "Index is of invalid size"
 
-Console.WriteLine "Vælg x af type int til 'SafeIndexTry'"
+Console.WriteLine "Vælg x af type int til 'safeIndexTry'"
 /// <remarks> Because indexValue was defined as a mutable, it can be changed by using the '<-' operator for new value assignment </remarks>
 indexValue <- int (Console.ReadLine())
-printfn "A.[%d]:  %A" indexValue (SafeIndexTry arrayEx indexValue)
+printfn "A.[%d]:  %A" indexValue (safeIndexTry arrayEx indexValue)
 
 
 /// <summary> SafeIndexOption : A function, which takes two inputs and evaluates using try/with - but here using the option type. In order for the function to output an integer, the Some and None option have to be encapsulated within 'Option.get' - this returns an integer if Some, otherwise the encountered runtime error </summar> 
 /// <param name="A"> A is an Array </param>
 /// <param name="x"> x is an integer, which specifies the index of the array </param>
-let SafeIndexOption (A : 'a[]) (x : int) =
+let safeIndexOption (A : 'a[]) (x : int) =
     try
         Option.get (Some(A.[x]))
     with
         _ -> Option.get (None)
 
-Console.WriteLine "Vælg x af type int til 'SafeIndexOption'"
+Console.WriteLine "Vælg x af type int til 'safeIndexOption'"
 indexValue <- int (Console.ReadLine())
-printfn "A.[%d]:  %A" indexValue (SafeIndexOption arrayEx indexValue)
+printfn "A.[%d]:  %A" indexValue (safeIndexOption arrayEx indexValue)
--- Assignments/7/7i/7i1.fsx
+++ Assignments/7/7i/7i1.fsx
@@ -1,4 +1,4 @@
-let thisFile = "filename_7i1.txt"     
+let thisFile = "afile.txt"     
 
 open System
 open System.IO
@@ -11,22 +11,35 @@ let rec readFile (stream : StreamReader) =
         (stream.ReadLine ()) + (readFile stream)
     else
         ""
+/// <summary> Build a help-function which writes a file </summmary>
+/// <param name="filename"> A string - contains a filename </param>
+/// <param name="text"> A string - a text corpus to write </param>
+let writeFile (filename : string) (text : string) =
+    let stream = File.CreateText filename
+    let writer = stream.WriteLine text
+    stream.Close ()
+
 /// <summary> Build a fileReplace using readFile, regex and WriteAllText </summary>
 /// <param name="filename"> A string - contains a filename </param>
 /// <param name="needle"> A string - the word to be replaced </param>
 /// <param name="replace"> A string - the replacement word </param>
+/// <remarks> This function will prompt the user with a before/after display of the textfile </remarks>
 let fileReplace (filename : string) (needle : string) (replace : string) =
     let inputStream = File.OpenText filename
     let corpus = readFile inputStream
     inputStream.Close()
+
     printfn "Filen ser indledningsvis således ud:\n%s" corpus
     let replacement = Regex.Replace(corpus, needle, replace)
-    File.WriteAllText(filename, replacement)
+    
+    writeFile filename replacement
     let inputStream = File.OpenText filename
     let newcorpus = readFile inputStream
     inputStream.Close()
+    
     printfn "...og ser nu således ud:\n%s" newcorpus
 
+/// <remarks> Run the fileReplace-function two times to ensure, that the function works both ways </remarks>
 fileReplace thisFile "PoP" "Programmering og Problemløsning"
 Console.WriteLine "\nVi kan vende dette om - og dermed starte forfra\n"
 fileReplace thisFile "Programmering og Problemløsning" "PoP"
