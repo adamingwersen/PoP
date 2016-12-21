5eacb67b015a8723a8c01275d7339e7662ab77d6
adamingwersen <adamlinnemann@gmail.com>
2016-12-21 18:52:09 +0100

    added subclass

--- Assignments/10/10i.fsx
+++ Assignments/10/10i.fsx
@@ -1,14 +1,12 @@
 /// <summary> Define class 'Animal' </summary>
 
-type Animal(foodIntake: float, currentSpeed: float, maxSpeed: float) =
- 
- let mutable cSpeed = 0 
+type Animal(animal: char, foodIntake: float, currentSpeed: float, maxSpeed: float) =
 
  let rand = System.Random()
- let weight = rand.Next(70, 300)
- let foodPercentage = rand.Next(0, 100)
+ let (weight: float) = (float (rand.Next(70, 300)))
+ let (foodPercentage: float) = (float (rand.Next(0, 100)))
  
- let (anySpeed: float) = ((float foodPercentage)/100.0)*maxSpeed
+ let (anySpeed: float) = (foodPercentage/100.0)*maxSpeed
 
  let newIntake = (float weight)/2.0
 
@@ -18,6 +16,12 @@ type Animal(foodIntake: float, currentSpeed: float, maxSpeed: float) =
  member this.pSpeed = anySpeed
  member this.perc = foodPercentage
  member this.req = newIntake
+
+type Carnivore(foodIntake:float, currentSpeed:float, maxSpeed:float) =
+ inherit Animal()
+
+ let relIntake = weight*0.08
+ member this.relIntake = relIntake
 
 let testAnimal = new Animal(100.0, 0.0, 25.0)
 printfn "%A %A %A %A" testAnimal.pSpeed testAnimal.perc testAnimal.req testAnimal.pWeight
