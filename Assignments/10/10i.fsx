25b945aad3dda5048146857e73d3688d4db647d7
adamingwersen <adamlinnemann@gmail.com>
2016-12-23 09:46:16 +0100

    inheritance issues

--- Assignments/10/10i.fsx
+++ Assignments/10/10i.fsx
@@ -1,5 +1,5 @@
 /// <summary> Define class 'Animal' </summary>
-
+(*
 type Animal(animal: char, foodIntake: float, currentSpeed: float, maxSpeed: float) =
 
  let rand = System.Random()
@@ -9,6 +9,8 @@ type Animal(animal: char, foodIntake: float, currentSpeed: float, maxSpeed: floa
  let (anySpeed: float) = (foodPercentage/100.0)*maxSpeed
 
  let newIntake = (float weight)/2.0
+ abstract member specieSpeed: unit -> float
+ default this.specieSpeed() = weight
 
  member this.foodIntake = foodIntake
  member this.maxSpeed = maxSpeed
@@ -17,12 +19,40 @@ type Animal(animal: char, foodIntake: float, currentSpeed: float, maxSpeed: floa
  member this.perc = foodPercentage
  member this.req = newIntake
 
-type Carnivore(foodIntake:float, currentSpeed:float, maxSpeed:float) =
+type Carnivore(animal: char, foodIntake:float, currentSpeed:float, maxSpeed:float) =
  inherit Animal()
 
- let relIntake = weight*0.08
- member this.relIntake = relIntake
+ override this.specieSpeed() = base.specieSpeed()*0.08
+*)
 
-let testAnimal = new Animal(100.0, 0.0, 25.0)
-printfn "%A %A %A %A" testAnimal.pSpeed testAnimal.perc testAnimal.req testAnimal.pWeight
+type Animal(weight:float, maxSpeed: float) = 
+ 
+ let rand = System.Random()  
+ 
+ let mutable foodPercentage = (float (rand.Next(0, 100)))
+ 
+ member this.weight() = (float (rand.Next(70, 300)))
 
+ member this.currentSpeed() =
+  foodPercentage*maxSpeed
+
+ member this.foodNeeded() = maxSpeed*0.5
+
+(*type Carnivore(maxSpeed: float) =
+ inherit Animal(maxSpeed: float)
+
+ override this.foodNeeded() = weight*0.08
+ 
+type Herbivore(maxSpeed: float) =
+ inherit Animal(maxSpeed: float)
+
+ override this.foodNeeded() = weight*0.4
+
+*)
+let testAnimal = new Animal(0.0, 50.0)
+printfn "%A %A" testAnimal.foodNeeded testAnimal.currentSpeed
+printfn "%A" testAnimal.foodNeeded
+(*
+let testCheetah = new Carnivore(100.0)
+printfn "%A %A" testCheetah.foodNeeded testCheetah.currentSpeed
+*)
